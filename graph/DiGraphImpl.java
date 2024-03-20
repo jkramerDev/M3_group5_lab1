3ba1c8ed0ec1a00f2cf91f63226d0320e734c6c5
tomams <tomams@tomass-mbp.fairfield.edu>
2024-03-20 15:01:52 -0400

    implemented methods

--- graph/DiGraphImpl.java
+++ graph/DiGraphImpl.java
@@ -7,6 +7,102 @@ import java.util.List;
 public class DiGraphImpl implements DiGraph{
 
 	private List<GraphNode> nodeList = new ArrayList<>();
+
+	@Override
+	public Boolean addNode(GraphNode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean removeNode(GraphNode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean setNodeValue(GraphNode node, String newNodeValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNodeValue(GraphNode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean addEdge(GraphNode fromNode, GraphNode toNode, Integer weight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean removeEdge(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean setEdgeValue(GraphNode fromNode, GraphNode toNode, Integer newWeight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getEdgeValue(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GraphNode> getAdjacentNodes(GraphNode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean nodesAreAdjacent(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean nodeIsReachable(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean hasCycles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GraphNode> getNodes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GraphNode getNode(String nodeValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int fewestHops(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int shortestPath(GraphNode fromNode, GraphNode toNode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 	
--- tree/OrgChartImpl.java
+++ tree/OrgChartImpl.java
@@ -8,6 +8,42 @@ public class OrgChartImpl implements OrgChart{
 
 	//Employee is your generic 'E'..
 	private List<GenericTreeNode<Employee>> nodes = new ArrayList<>();
+
+	@Override
+	public void addRoot(Employee e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addDirectReport(Employee manager, Employee newPerson) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEmployee(Employee firedPerson) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showOrgChartDepthFirst() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showOrgChartBreadthFirst() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
