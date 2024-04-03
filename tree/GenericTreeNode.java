5e2a550c1f96d0b322d5e34a6014ded6a3a833d8
ACrandell298 <andrewcrandell298@gmail.com>
2024-04-03 14:48:04 -0400

    test

--- tree/GenericTreeNode.java
+++ tree/GenericTreeNode.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class GenericTreeNode<E> {
 	E data;
 	//<some list of children>
-	ArrayList<GenericTreeNode<E>> children;
+	ArrayList<GenericTreeNode<E>> children = new ArrayList<>();
 	
 	public GenericTreeNode(E theItem) {
 		data = theItem;
